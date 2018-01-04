8dbfd51c41a47af43524a2b1970f9188cc5f8300
arifams <arif.amsterdam@gmail.com>
2018-01-04 11:00:48 +0100

    add redux, thunk and middleware for root reducers

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'semantic-ui-css/semantic.min.css';
+import rootReducer from "./rootReducer";
+
+// Because we are gonna use combine reducer to split function later
+// rootReducer for whole tree and middleware for thunk redux
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>, 
   document.getElementById('root')
 );
